b94a2ad13f49abe2800ef2fc881b10e0f8c12d16
Ivan Perez <ivan.perez@keera.es>
2012-04-02 13:37:40 +0200

    Regulates speed with a slider.

--- src/Controller/Conditions/Speed.hs
+++ src/Controller/Conditions/Speed.hs
@@ -1,9 +1,10 @@
 module Controller.Conditions.Speed where
 
-import Data.CBRef
 import Control.Monad
+import GHC.Float
 import Graphics.UI.Gtk
 import Hails.MVC.Model.ProtectedModel.Reactive
+import Hails.MVC.Controller.ConditionDirection
 
 import CombinedEnvironment
 import Graphics.MultiCoreStatus
@@ -13,6 +14,8 @@ import Model.Model
 installHandlers :: CEnv -> IO()
 installHandlers cenv = void $ do
   let ui = uiBuilder $ view cenv
+      pm = model cenv
+
   pause <- pauseToolBtn ui
   pause `onToolButtonClicked` conditionPause cenv
 
@@ -27,6 +30,11 @@ installHandlers cenv = void $ do
 
   slowDown <- slowDownToolBtn ui
   slowDown `onToolButtonClicked` conditionSlowDown cenv
+
+  hscale <- hscale1 ui
+  hscale `on` valueChanged $ conditionSpeedChanged VM cenv
+  onEvent pm SpeedChanged $ conditionSpeedChanged MV cenv
+  onEvent pm Initialised $ conditionSpeedChanged MV cenv
 
 conditionPause :: CEnv -> IO()
 conditionPause cenv = do
@@ -58,3 +66,21 @@ conditionSlowDown cenv = do
   curSp <- getter speedField pm
   when (curSp >= 0.2) $ setter speedField pm (curSp / 2)
  where pm = model cenv
+
+-- To be implemented using (=:=)
+conditionSpeedChanged :: ConditionDirection -> CEnv -> IO()
+conditionSpeedChanged cd cenv = do
+  -- View value
+  hscale <- hscale1 ui
+
+  -- Model value
+  curSp <- getter speedField pm
+  curV  <- fmap double2Float $ get hscale rangeValue 
+
+  when (curV /= curSp) $
+    case cd of
+     MV -> set hscale [ rangeValue := float2Double curSp ]
+     VM -> setter speedField pm curV
+
+ where pm = model cenv
+       ui = uiBuilder $ view cenv
