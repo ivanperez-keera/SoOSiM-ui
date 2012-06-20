eb82d7ca80e960db5fa83ad1a24cb86ad1eb9f57
Ivan Perez <ivan.perez@keera.es>
2012-06-20 09:10:03 +0100

    A more general implementation of tooltip installation.

--- src/View/Tooltips.hs
+++ src/View/Tooltips.hs
@@ -1,38 +1,55 @@
+-- | Includes a function to initialise the tooltips and return them.
+--
+--  FIXME: the function receives a tooltips object and an UI object and, using a table
+--  of UI Accessors, it installs the tooltips.
 module View.Tooltips where
 
 import Graphics.UI.Gtk
 import View.Objects
 
+-- Creates a Tooltips element using a builder and a known table of accessors to
+-- widgets from the builder.
+--
+-- FIXME: Move to hails || to hails.view.defaultView
 initialiseTooltips :: Builder -> IO Tooltips
 initialiseTooltips bldr = do
- tlt <- tooltipsNew
+  tlts <- tooltipsNew
+  initialiseTooltips' bldr tlts
+  tooltipsEnable tlts
+  return tlts
+
+-- Installs tooltips on a Tooltips element using a builder and a known table of
+-- accessors to widgets from the builder.
+--
+-- FIXME: Move to hails?
+initialiseTooltips' :: Builder -> Tooltips -> IO ()
+initialiseTooltips' bldr tlts = do
+  mapM_ (installTooltip bldr tlts) tooltipsWithIds
+ where tooltipsWithIds = zip tooltipsTable identifiers
+       identifiers     = map (("T" ++) . show) [1..]
  
- run <- runToolBtn bldr
- tooltipsSetTip tlt run "Run" "T1"
+-- Installs one tooltip on a tooltips widget using a builder, an accessor from that builder,
+-- a tooltip string and an identifier
+--
+-- FIXME: Move to hails?
+installTooltip :: Builder -> Tooltips -> ((Builder -> IO Widget, String), String) -> IO ()
+installTooltip bldr tlts ((accessor, string), identifier) = do
+  widget <- accessor bldr
+  tooltipsSetTip tlts widget string identifier
 
- sl <- slowDownToolBtn bldr
- tooltipsSetTip tlt sl "Decrease simulation speed" "T2"
-
- sp <- speedUpToolBtn bldr
- tooltipsSetTip tlt sp "Increase simulation speed" "T3"
-
- st <- stopToolBtn bldr
- tooltipsSetTip tlt st "Stop" "T4"
-
- ps <- pauseToolBtn bldr
- tooltipsSetTip tlt ps "Pause" "T5"
-
- se <- stepForwardToolBtn bldr
- tooltipsSetTip tlt se "One step forward" "T6"
-
- fs <- fullScreenToolBtn bldr
- tooltipsSetTip tlt fs "Toggle fullscreen" "T7"
-
- fsM <- fullScreenMenuItem bldr
- tooltipsSetTip tlt fsM "Toggle fullscreen" "T8"
-
- sc <- hscale1 bldr
- tooltipsSetTip tlt sc "Simulation speed (steps/second)" "T9"
-
- tooltipsEnable tlt
- return tlt
+-- | The list of tooltips we know about and use
+tooltipsTable :: [(Builder -> IO Widget, String)]
+tooltipsTable =
+ [ (asWidget runToolBtn,         "Run"                             )
+ , (asWidget slowDownToolBtn,    "Decrease simulation speed"       )
+ , (asWidget speedUpToolBtn,     "Increase simulation speed"       )
+ , (asWidget stopToolBtn,        "Stop"                            )
+ , (asWidget pauseToolBtn,       "Pause"                           )  
+ , (asWidget stepForwardToolBtn, "One step forward"                ) 
+ , (asWidget fullScreenToolBtn,  "Toggle fullscreen"               )
+ , (asWidget hscale1,            "Simulation speed (steps/second)" )
+ ]
+ where -- Transforms an accessor of a type in WidgetClass into a widget
+       -- accessor
+       asWidget :: WidgetClass a => (b -> IO a) -> (b -> IO Widget)
+       asWidget x = fmap toWidget . x
