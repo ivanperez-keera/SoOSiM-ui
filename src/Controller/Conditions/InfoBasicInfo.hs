8b5a3b628a6746699e09e27b3d89e959b5825741
Ivan Perez <ivan.perez@keera.es>
2013-06-13 14:23:19 +0200

    Does not reuse text buffers

--- src/Controller/Conditions/InfoBasicInfo.hs
+++ src/Controller/Conditions/InfoBasicInfo.hs
@@ -27,22 +27,30 @@ installHandlers cenv = void $
   
 -- | Shows component info of the selected component
 conditionShowCompInfo :: CEnv -> IO()
-conditionShowCompInfo cenv = do
+conditionShowCompInfo cenv = onViewAsync $ do
  stM  <- getter simStateField (model cenv) -- readCBMVar $ mcs $ view cenv
  awhen stM $ \st -> do
-   bf1 <- textViewGetBuffer <=< infoTextView  $ view cenv
-   bf2 <- textViewGetBuffer <=< traceTextView $ view cenv
+   -- bf1 <- textViewGetBuffer <=< infoTextView  $ view cenv
+   -- bf2 <- textViewGetBuffer <=< traceTextView $ view cenv
 
    let sel   = selection $ simGLSystemStatus st
        mcs   = present $ multiCoreStatus $ simGLSystemStatus st
 
    case (sel, getElemInfo sel mcs) of
-     ([], _) -> do textBufferUpdateText bf1 "" 
-                   textBufferUpdateText bf2 ""
-     (_, Nothing) -> do textBufferUpdateText bf1 "The selected component has ceased to exist" 
-                        textBufferUpdateText bf2 ""
-     (_, Just (ni,nt)) -> do textBufferUpdateText bf1 ni
-                             textBufferUpdateText bf2 nt
+     ([], _) -> do textViewUpdateText cenv infoTextView "" 
+                   textViewUpdateText cenv traceTextView ""
+     (_, Nothing) -> do textViewUpdateText cenv infoTextView  "The selected component has ceased to exist" 
+                        textViewUpdateText cenv traceTextView ""
+     (_, Just (ni,nt)) -> do textViewUpdateText cenv infoTextView  ni
+                             textViewUpdateText cenv traceTextView nt
+
+-- | Auxiliary function that updates a text view. We need to use this function
+-- instead of calling textBufferUpdateText because buffers cannot be reused.
+-- textViewUpdateText :: (TextViewClass b, GtkGUI a)
+--                    => CEnv -> (a -> IO b) -> String -> IO ()
+textViewUpdateText cenv f text = do
+  bf <- textViewGetBuffer <=< f $ view cenv
+  textBufferUpdateText bf text
 
 -- | Renders the info relative to a given element (both basic info and a trace)
 getElemInfo :: [Name]                 -- ^ The qualified name of the element whose info we need
