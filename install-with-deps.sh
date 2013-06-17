0dfe34068ea138c306142e903663da7405ea8a1d
Ivan Perez <ivan.perez@keera.es>
2013-06-18 00:09:00 +0300

    Adds gtk2hs-buildtools. Changes installation order.

--- install-with-deps.sh
+++ install-with-deps.sh
@@ -15,6 +15,7 @@ git clone --depth=1 git://github.com/ivanperez-keera/SoOSiM-components.git
 git clone --depth=1 git://github.com/christiaanb/SoOSiM.git
 
 export PATH=$PATH:$PWD/cabal-dev/bin
+cabal-dev install gtk2hs-buildtools
 cabal-dev add-source gloss-gtk
 cabal-dev add-source gtk-helpers
 cabal-dev add-source MissingK
@@ -26,8 +27,8 @@ cabal-dev add-source hails-mvc-view-gtk
 cabal-dev add-source hails-mvc-model-protectedmodels
 cabal-dev add-source hails-mvc-environment-gtk
 cabal-dev add-source keera-hails
+cabal-dev install keera-hails
 cabal-dev add-source SoOSiM
 cabal-dev add-source SoOSiM-components
 cabal-dev add-source SoOSiM-ui
-cabal-dev install keera-hails
 cabal-dev install SoOSiM-ui
